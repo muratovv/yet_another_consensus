3303ad61b5b501e561f7338c872b77e4093a2e89
Fedor Muratov <muratovfyodor@yandex.ru>
2019-07-01 13:30:16 +0300

    Add stub of consensus initialization

---
+++ src/main/kotlin/main/initialization/ConsensusInitialization.kt
@@ -0,0 +1,9 @@
+package main.initialization
+
+import main.pipeline.AgreementGate
+
+class ConsensusInitialization {
+    fun initialize(): AgreementGate {
+        TODO("add initialization of agreement gate")
+    }
+}
